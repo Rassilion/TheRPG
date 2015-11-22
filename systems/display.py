fb1ee53f60321750bc8236503aaba1093dbbc43b
Rassilion <denizgezgin365@gmail.com>
2015-11-22 22:34:50 +0200

    fix

--- systems/display.py
+++ systems/display.py
@@ -7,10 +7,10 @@ class Display(System):
     def update(self, dt):
         for e, c in self.entity_manager.pairs_for_type(Position):
             try:
-                name = self.entity_manager.component_for_entity(e, Name)
+                name = self.entity_manager.component_for_entity(e, Name).name
                 hp = self.entity_manager.component_for_entity(e, Health).hp
             except NonexistentComponentTypeForEntity:
                 name = e
                 hp = 0
 
-            print("{} position, hp {}: {!r}".format(name.name, hp, c.pos))
+            print("{} position, hp {}: {!r}".format(name, hp, c.pos))
